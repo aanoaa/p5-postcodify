0444f1d4557772158557a44f542853c94289b0df
Hyungsuk Hong <aanoaa@gmail.com>
2014-11-20 04:57:57 +0900

    dbicdump - ready to use DBIC

---
+++ lib/Postcodify/Schema/Result/PostcodifyKeyword.pm
@@ -0,0 +1,76 @@
+use utf8;
+package Postcodify::Schema::Result::PostcodifyKeyword;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+Postcodify::Schema::Result::PostcodifyKeyword
+
+=cut
+
+use strict;
+use warnings;
+
+=head1 BASE CLASS: L<Postcodify::Schema::Base>
+
+=cut
+
+use base 'Postcodify::Schema::Base';
+
+=head1 TABLE: C<postcodify_keywords>
+
+=cut
+
+__PACKAGE__->table("postcodify_keywords");
+
+=head1 ACCESSORS
+
+=head2 seq
+
+  data_type: 'integer'
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 address_id
+
+  data_type: 'integer'
+  is_nullable: 0
+
+=head2 keyword_crc32
+
+  data_type: 'integer'
+  is_nullable: 1
+  size: 10
+
+=cut
+
+__PACKAGE__->add_columns(
+  "seq",
+  { data_type => "integer", is_auto_increment => 1, is_nullable => 0 },
+  "address_id",
+  { data_type => "integer", is_nullable => 0 },
+  "keyword_crc32",
+  { data_type => "integer", is_nullable => 1, size => 10 },
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</seq>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("seq");
+
+
+# Created by DBIx::Class::Schema::Loader v0.07040 @ 2014-11-20 04:57:07
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:ewLC+Z7U/cLF723vd9ue6Q
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;
---
+++ lib/Postcodify/Schema/Result/PostcodifyPobox.pm
@@ -0,0 +1,108 @@
+use utf8;
+package Postcodify::Schema::Result::PostcodifyPobox;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+Postcodify::Schema::Result::PostcodifyPobox
+
+=cut
+
+use strict;
+use warnings;
+
+=head1 BASE CLASS: L<Postcodify::Schema::Base>
+
+=cut
+
+use base 'Postcodify::Schema::Base';
+
+=head1 TABLE: C<postcodify_pobox>
+
+=cut
+
+__PACKAGE__->table("postcodify_pobox");
+
+=head1 ACCESSORS
+
+=head2 seq
+
+  data_type: 'integer'
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 address_id
+
+  data_type: 'integer'
+  is_nullable: 0
+
+=head2 keyword
+
+  data_type: 'varchar'
+  is_nullable: 1
+  size: 40
+
+=head2 range_start_major
+
+  data_type: 'integer'
+  is_nullable: 1
+  size: 5
+
+=head2 range_start_minor
+
+  data_type: 'integer'
+  is_nullable: 1
+  size: 5
+
+=head2 range_end_major
+
+  data_type: 'integer'
+  is_nullable: 1
+  size: 5
+
+=head2 range_end_minor
+
+  data_type: 'integer'
+  is_nullable: 1
+  size: 5
+
+=cut
+
+__PACKAGE__->add_columns(
+  "seq",
+  { data_type => "integer", is_auto_increment => 1, is_nullable => 0 },
+  "address_id",
+  { data_type => "integer", is_nullable => 0 },
+  "keyword",
+  { data_type => "varchar", is_nullable => 1, size => 40 },
+  "range_start_major",
+  { data_type => "integer", is_nullable => 1, size => 5 },
+  "range_start_minor",
+  { data_type => "integer", is_nullable => 1, size => 5 },
+  "range_end_major",
+  { data_type => "integer", is_nullable => 1, size => 5 },
+  "range_end_minor",
+  { data_type => "integer", is_nullable => 1, size => 5 },
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</seq>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("seq");
+
+
+# Created by DBIx::Class::Schema::Loader v0.07040 @ 2014-11-20 04:57:07
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:4ui6QykE9AHEakb647xSVw
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;
