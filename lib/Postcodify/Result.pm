351ef1fbaceb6e37bed61d4c670309ba2e833c4d
Hyungsuk Hong <aanoaa@gmail.com>
2017-02-26 18:21:28 +0900

    Remove variable prefix 'address_'

--- lib/Postcodify/Result.pm
+++ lib/Postcodify/Result.pm
@@ -26,84 +26,84 @@ sub _trigger_resultset {
 
     while ( my $row = $self->resultset->next ) {
         ## 한글 도로명 및 지번주소를 정리한다.
-        my $address_ko_base = trim(
+        my $ko_base = trim(
             sprintf "%s %s %s %s",
             $row->road->sido_ko     || '',
             $row->road->sigungu_ko  || '',
             $row->road->ilbangu_ko  || '',
             $row->road->eupmyeon_ko || ''
         );
-        my $address_ko_new = trim(
+        my $ko_new = trim(
             sprintf "%s %s %s%s",
             $row->road->road_name_ko || '',
             $row->is_basement ? '지하' : '',
             $row->num_major || '',
             $row->num_minor ? '-' . $row->num_minor : ''
         );
-        my $address_ko_old = trim(
+        my $ko_old = trim(
             sprintf "%s %s %s%s",
             $row->dongri_ko || '',
             $row->is_mountain ? '산' : '',
             $row->jibeon_major || '',
             $row->jibeon_minor ? '-' . $row->jibeon_minor : ''
         );
-        $address_ko_base =~ s/ {2,}/ /g;
-        $address_ko_new =~ s/ {2,}/ /g;
-        $address_ko_old =~ s/ {2,}/ /g;
+        $ko_base =~ s/ {2,}/ /g;
+        $ko_new =~ s/ {2,}/ /g;
+        $ko_old =~ s/ {2,}/ /g;
 
         ## 영문 도로명 및 지번주소를 정리한다.
-        my $address_en_base = trim(
+        my $en_base = trim(
             sprintf "%s %s %s %s",
             $row->road->eupmyeon_en || '',
             $row->road->ilbangu_en  || '',
             $row->road->sigungu_en  || '',
             $row->road->sido_en     || ''
         );
-        my $address_en_new = trim(
+        my $en_new = trim(
             sprintf "%s %s%s %s",
             $row->is_basement ? 'Jiha' : '',
             $row->num_major || '',
             $row->num_minor ? '-' . $row->num_minor : '',
             $row->road->road_name_en || ''
         );
-        my $address_en_old = trim(
+        my $en_old = trim(
             sprintf "%s %s%s %s",
             $row->is_mountain ? 'San' : '',
             $row->jibeon_major || '',
             $row->jibeon_minor ? '-' . $row->jibeon_minor : '',
             $row->dongri_en || ''
         );
-        $address_en_base =~ s/ {2,}/ /g;
-        $address_en_new =~ s/ {2,}/ /g;
-        $address_en_old =~ s/ {2,}/ /g;
+        $en_base =~ s/ {2,}/ /g;
+        $en_new =~ s/ {2,}/ /g;
+        $en_old =~ s/ {2,}/ /g;
 
         ## 추가정보를 정리한다.
         my ( $extra_info_long, $extra_info_short, $other_addresses );
         if ( $self->sort eq 'POBOX' ) {
-            $address_ko_new  = $address_ko_old   = $row->dongri_ko . ' ' . $row->other_addresses;
-            $address_en_new  = $address_en_old   = $row->dongri_en . ' ' . $row->other_addresses;
+            $ko_new          = $ko_old           = $row->dongri_ko . ' ' . $row->other_addresses;
+            $en_new          = $en_old           = $row->dongri_en . ' ' . $row->other_addresses;
             $extra_info_long = $extra_info_short = $other_addresses = '';
         }
         else {
-            $extra_info_long = trim( join( '', $address_ko_old, $row->building_name || '' ) );
+            $extra_info_long = trim( join( '', $ko_old, $row->building_name || '' ) );
             $extra_info_short = trim( join( '', $row->dongri_ko || '', $row->building_name || '' ) );
             $other_addresses = $row->other_addresses;
         }
 
         my $data = {
-            dbid    => $row->address_id,
+            dbid    => $row->building_id,
             code6   => substr( $row->postcode6, 0, 3 ) . '-' . substr( $row->postcode6, 3, 3 ),
             code5   => $row->postcode5,
             address => {
-                base     => $address_ko_base,
-                new      => $address_ko_new,
-                old      => $address_ko_old,
+                base     => $ko_base,
+                new      => $ko_new,
+                old      => $ko_old,
                 building => $row->building_name
             },
             english => {
-                base     => $address_en_base,
-                new      => $address_en_new,
-                old      => $address_en_old,
+                base     => $en_base,
+                new      => $en_new,
+                old      => $en_old,
                 building => ''
             },
             other => {
