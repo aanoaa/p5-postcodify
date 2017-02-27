3dd84957aa09faa44569c85699041f9f93a31dac
Hyungsuk Hong <aanoaa@gmail.com>
2017-02-27 16:01:56 +0900

    Update scripts/query.pl

--- scripts/query.pl
+++ scripts/query.pl
@@ -2,8 +2,6 @@ use utf8;
 use strict;
 use warnings;
 use Encode 'decode_utf8';
-use Data::Dump;
-use Data::Printer;
 
 use lib 'lib';
 use Postcodify::Query;
@@ -16,4 +14,11 @@ binmode STDERR, ':utf8';
 my $q = Postcodify::Query->new;
 $q->parse( decode_utf8( join( ' ', @ARGV ) ) );
 print "$q\n";
-p $q;
+print "---------------------\n";
+for my $key (qw/sido sigungu ilbangu eupmyeon dongri road pobox use_area lang sort/) {
+    printf( "%s: %s\n", $key, $q->$key || '' );
+}
+
+for my $key (qw/numbers buildings/) {
+    printf( "%s: %s\n", $key, join( ', ', @{ $q->$key } ) );
+}
